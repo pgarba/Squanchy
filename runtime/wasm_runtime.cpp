5725a9af679d47d24f7e5280bd725afe759186d9
adam <adam@mac>
2025-01-14 13:55:57 +0800

    Improve wasm runtime

--- runtime/wasm_runtime.cpp
+++ runtime/wasm_runtime.cpp
@@ -1,17 +1,15 @@
 /*
-    Helpers to unfold the lifted wasm2c code and deobfuscate it.
+    Some helpers needed to unfold the lifted wasm2c code and let it fold
 */
 #include <cstdint>
 
+// The wasm runtime header.
 #include "wasm-rt.h"
 
+// The default page size is 64KB.
 #define PAGE_SIZE 65536
 
 extern "C" {
-
-extern void init_globals(void *w2cInstance);
-extern void init_memories(void *w2cInstance);
-extern void init_data_instances(void *w2cInstance);
 
 /*
     Allocate memory for the wasm runtime.
@@ -25,26 +23,13 @@ wasm_rt_allocate_memory(wasm_rt_memory_t *memory, uint64_t initial_pages,
   memory->data = (uint8_t *)calloc(memory->size, 1);
 }
 
+/*
+   Free memory for the wasm runtime.
+*/
 void __attribute__((always_inline))
 wasm_rt_free_memory(wasm_rt_memory_t *memory) {
   free(memory->data);
+  memory->data = nullptr;
 }
 
-/*
-    Call the wasm2c initializers to initialize the instance.
-*/
-/*
-void *__attribute__((always_inline)) Init(void *w2cInstance) {
-  // 1. init_globals
-  init_globals(w2cInstance);
-
-  // 2. init_memories
-  init_memories(w2cInstance);
-
-  // 3. init_data_instances
-  init_data_instances(w2cInstance);
-
-  return w2cInstance;
-};
-*/
-};
+}; // extern "C"
